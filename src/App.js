ea01f00ddff2a3c3f69f0b0770f8a99121296527
bhavesh848785 <bhaveshvaghela4545@gmail.com>
2024-02-14 23:30:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  //hooks.....................................................
+  const [inputlist, setinputlist] = useState();
+
+  const [items, setitems] = useState([]);
+
+  //input event.........................................
+  const inputEvent = (event) => {
+    setinputlist(event.target.value);
+  };
+
+  //display event..........................................
+  const listOfitems = (event) => {
+    // event.preventDefault();
+    setitems((olditems) => {
+      return [...olditems, inputlist];
+    });
+    setinputlist("");
+  };
+
+  //delete event...................................
+  const deleteItem = (index) => {
+    setitems((olditems) => {
+      return olditems.filter((item, i) => i !== index);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input
+          type="text"
+          value={inputlist}
+          placeholder="add the list"
+          onChange={inputEvent}
+        />
+        <button on onClick={listOfitems}>
+          +
+        </button>
+
+        <ol>
+          {/* <li>{inputlist}</li> */}
+
+          {items.map((itemval, index) => {
+            return (
+              <>
+                <div className="todo_style" key={index}>
+                  {/* <i class="fa fa-times" area-hidden="true" /> */}
+                  <i
+                    className="fa-solid fa-circle-xmark"
+                    area-hidden="true"
+                    onClick={() => deleteItem(index)}
+                  ></i>
+                  <li>{itemval}</li>
+                </div>
+              </>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
